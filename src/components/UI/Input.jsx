dd1fc637c4613871d044cac9b275bb1d5ffaf300
Bilal <b1335190@gmail.com>
2023-06-22 17:40:44 +0600

    'updated/input'

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Input = ({ value, placeholder, ...rest }) => {
-   return <StyledInput value={value} placeholder={placeholder} {...rest} />
+   return (
+      <StyledInput value={value} placeholder="telephone bir nerse" {...rest} />
+   )
 }
 
 export default Input
 
 const StyledInput = styled.input`
    outline: none;
-   width: 561px;
+   width: 560px;
    height: 55px;
    border: 2px solid #0a5271;
    border-radius: 3px;
    font-family: 'Roboto';
    font-style: normal;
    font-weight: 400;
-   font-size: 20px;
+   font-size: 16px;
    line-height: 19px;
    color: #828282;
+   padding-left: 16px;
    @media only screen and (max-width: 390px) {
-      width: 360px;
+      max-width: 90%;
       height: 34px;
+      font-size: 14px;
    }
 `
