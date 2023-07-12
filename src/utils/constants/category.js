8d6769770810f855c23a29719983b2dea0914c98
Your Name <zukokalilov@gmail.com>
2023-07-12 16:45:21 +0600

    created project students

---
+++ src/utils/constants/category.js
@@ -0,0 +1,26 @@
+import image from '../../assets/image 11.svg'
+
+export const sliderArray = [
+   {
+      name: 'GIFT.ME',
+      text: 'Gift.me – это веб-сервис, где пользователи могут размещать свои желаемые подарки и  делать подарки другим.',
+      img: image,
+      list: [
+         {
+            name: '2 Проектных менеджера',
+         },
+         {
+            name: '2 UI-UX desiner',
+         },
+         {
+            name: '3 Front-end разработчика',
+         },
+         {
+            name: '2 Back-end разработчика',
+         },
+         {
+            name: '1 Тестировщик',
+         },
+      ],
+   },
+]
